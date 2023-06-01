c2e20b8fe538ddc08132f0e04125b5e7c4c526d3
estercardosotoja <ester.toja15@gmail.com>
2023-06-01 18:04:06 -0300

    hosts

--- setup/settings.py
+++ setup/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.221.46.131', 'localhost']
 
 
 # Application definition
